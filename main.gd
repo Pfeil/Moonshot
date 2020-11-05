c4f9e457de897888427ac039c5a7211a2c2b77e4
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2020-11-05 22:23:06 +0100

    Initial Commit

---
+++ main.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+onready var planet_a = self.get_node("Planet")
+onready var planet_b = self.get_node("Planet2")
+onready var planet_c = self.get_node("Planet3")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var factor = 20
+	self.planet_a.apply_central_impulse(Vector2.UP   * factor)
+	self.planet_b.apply_central_impulse(Vector2.DOWN * factor)
+	self.planet_c.apply_central_impulse(Vector2.DOWN * factor)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
