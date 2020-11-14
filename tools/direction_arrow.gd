07fe113c6ba377cd0f3ed345a183e4eb2fa07111
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2020-11-14 22:03:56 +0100

    Direction arrow can now function as percentage bar.

--- tools/direction_arrow.gd
+++ tools/direction_arrow.gd
@@ -1,5 +1,10 @@
 extends Line2D
 
+onready var end: Vector2 = self.points[1]
 
 func set_direction(direction: Vector2):
 	self.points[1] = direction
+	
+func set_percent(percent: float):
+	var begin: Vector2 = self.points[0]
+	self.points[1] = begin.linear_interpolate(self.end, percent)
