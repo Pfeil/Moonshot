9f077fc0a596b1e628ddace23504413512fc2c37
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2020-11-14 20:59:23 +0100

    Walking on Planets works with new players.

--- players/base.gd
+++ players/base.gd
@@ -3,27 +3,38 @@ extends KinematicBody2D
 
 export var speed: float = 5.0;
 export var kinematic_gravity: float = 2.0
-export var sticky_vector_length: float = 10.0
+export var sticky_vector_length: float = 35.0
 
 var direction: Vector2 = Vector2.ZERO
+var sticky_vector: Vector2 = Vector2.DOWN * sticky_vector_length
 
 onready var control_arrow = self.get_node("control_vector")
 onready var down_arrow = self.get_node("sticky_vector")
-onready var up_arrow = self.get_node("up_vector")
+onready var velocity_arrow = self.get_node("linear_velocity_vector")
 
 func _ready():
 	pass # Replace with function body.
 
 
-func _physics_process(delta):
-	var down_vector: Vector2 = (Vector2.DOWN * self.kinematic_gravity).rotated(self.rotation)
-	var up_vector: Vector2 = -down_vector
+func _physics_process(delta: float):
+	#var down_vector: Vector2 = (Vector2.DOWN * self.sticky_vector_length).rotated(-self.rotation)
+	var up_vector: Vector2 = -self.sticky_vector
+	var direction: Vector2 = self.direction.rotated(self.rotation)
+	
+	var velocity: Vector2 = self.move_and_slide_with_snap(direction, self.sticky_vector, up_vector, true, 4, 0.785, false)
+		
+	for i in self.get_slide_count():
+		var collision: KinematicCollision2D = self.get_slide_collision(i)
+		if collision.collider.has_method("is_in_group") and collision.collider.is_in_group("walkables"):
+			#print("I collided with ", collision.collider.name)
+			var collider: Node2D = collision.collider
+			self.sticky_vector = collider.position - self.position
+			self.rotation = self.sticky_vector.angle() - PI/2
 	
 	self.control_arrow.set_direction(self.direction)
-	self.down_arrow.set_direction(down_vector)
-	self.up_arrow.set_direction(up_vector)
-	
-	self.move_and_slide_with_snap(self.direction, down_vector, up_vector)
+	self.down_arrow.set_direction(self.sticky_vector.rotated(-self.rotation))
+	self.velocity_arrow.set_direction(velocity.rotated(-self.rotation))
+
 
 func _input(event):
 	self.direction.y = self.kinematic_gravity
