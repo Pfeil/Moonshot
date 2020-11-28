4614eb16b5ea4f0f3c621989c06d573b74b346d1
Johannes Pfeil <Johannes.Pfeil@gmx.de>
2020-11-28 20:43:27 +0100

    Add controller support for multiple players

--- planet_player/planet_player.gd
+++ planet_player/planet_player.gd
@@ -4,11 +4,13 @@ export var PLAYER_NUMBER: int = 0
 export var bullet_impulse: float = 100
 export var hasRecoil: bool = true
 
+export var is_controlled_by_mouse: bool = false
 export var JOYPAD_NUMBER = 0
 export var JOYPAD_DEADZONE = 0.25
 
 
 var aiming_direction: Vector2
+onready var joypad_left:Vector2 = Vector2.ZERO
 onready var OS_NAME = OS.get_name()
 onready var rotation_point = $RotationPoint
 onready var cannon = $RotationPoint/Cannon
@@ -16,23 +18,26 @@ onready var bullet_spawn_position = $RotationPoint/Cannon/BulletSpawnPosition
 onready var bullet = preload("res://planet_player/bullet.tscn")
 
 
-
 func _process(_delta):
-	var analogStick_vectors = get_joystick_input()
-	if analogStick_vectors[0]:	#Joystick controlls
-		aiming_direction = analogStick_vectors[0].normalized()
-	elif analogStick_vectors[1]:
-		aiming_direction = analogStick_vectors[1].normalized()
-	else:	# Mouse controlls
+	if is_controlled_by_mouse:
 		aiming_direction = (get_global_mouse_position() - transform.get_origin())
+		if Input.is_action_just_pressed("ui_select"):
+			shoot()
+	else:
+		var analogStick_vectors = get_joystick_input()
+		if analogStick_vectors[0]:	#Joystick controlls
+			aiming_direction = analogStick_vectors[0].normalized()
+		elif analogStick_vectors[1]:
+			aiming_direction = analogStick_vectors[1].normalized()
 	rotation_point.look_at(transform.get_origin() + aiming_direction)
 
 func _physics_process(delta):
 	rotation = 0
 
 func _input(event):
-	if event.is_action_pressed("Player_"+str(PLAYER_NUMBER)+"_SHOOT"):
-		shoot()
+	if not is_controlled_by_mouse:
+		if event.is_action_pressed("Player_"+str(PLAYER_NUMBER)+"_SHOOT"):
+			shoot()
 
 
 func shoot():
@@ -46,6 +51,7 @@ func shoot():
 	bullet_instance.apply_central_impulse(Vector2(bullet_impulse, 0).rotated(rotation_point.rotation))
 	self.apply_central_impulse(-Vector2(bullet_impulse, 0).rotated(rotation_point.rotation))
 
+
 func get_joystick_input():
 	var leftAnalogStick_vector =Vector2.ZERO
 	var rightAnalogStick_vector =Vector2.ZERO
@@ -53,17 +59,17 @@ func get_joystick_input():
 	# TODO: not efficient to ask for OS every time.
 	# get input from left stick dependent of OS system
 	if OS_NAME == "Windows":
-		leftAnalogStick_vector = Vector2(Input.get_joy_axis(0, 0), -Input.get_joy_axis(0, 1))
-		rightAnalogStick_vector = Vector2(Input.get_joy_axis(0, 2), Input.get_joy_axis(0, 3))
+		leftAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 0), -Input.get_joy_axis(PLAYER_NUMBER, 1))
+		rightAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 2), Input.get_joy_axis(PLAYER_NUMBER, 3))
 	elif OS_NAME == "X11":
-		leftAnalogStick_vector = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
-		rightAnalogStick_vector = Vector2(Input.get_joy_axis(0, 2), Input.get_joy_axis(0, 3))
+		leftAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 0), Input.get_joy_axis(PLAYER_NUMBER, 1))
+		rightAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 2), Input.get_joy_axis(PLAYER_NUMBER, 3))
 	elif OS_NAME == "OSX":
-		leftAnalogStick_vector = Vector2(Input.get_joy_axis(0, 1), Input.get_joy_axis(0, 2))
-		rightAnalogStick_vector = Vector2(Input.get_joy_axis(0, 3), Input.get_joy_axis(0, 4))
+		leftAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 1), Input.get_joy_axis(PLAYER_NUMBER, 2))
+		rightAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 3), Input.get_joy_axis(PLAYER_NUMBER, 4))
 	else:
-		leftAnalogStick_vector = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1))
-		rightAnalogStick_vector = Vector2(Input.get_joy_axis(0, 2), Input.get_joy_axis(0, 3))
+		leftAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 0), Input.get_joy_axis(PLAYER_NUMBER, 1))
+		rightAnalogStick_vector = Vector2(Input.get_joy_axis(PLAYER_NUMBER, 2), Input.get_joy_axis(PLAYER_NUMBER, 3))
 	# take Deadzone of the sticks in account
 	if leftAnalogStick_vector.length() < JOYPAD_DEADZONE:
 		leftAnalogStick_vector = Vector2(0, 0)
